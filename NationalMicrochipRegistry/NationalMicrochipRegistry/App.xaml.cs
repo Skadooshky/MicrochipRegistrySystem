1bdf9b1fa2f7d254c0c3e7908ea11e57f844672b
Skadooshky <shivarndavidson1@gmail.com>
2025-06-03 12:25:31 +1200

    Test

---
+++ NationalMicrochipRegistry/NationalMicrochipRegistry/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NationalMicrochipRegistry
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
